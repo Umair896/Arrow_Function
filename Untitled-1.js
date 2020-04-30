a8bf847520931eb1612d9205cf1c9db73e2fdbb6
Umair896 <umairansari896@gmail.com>
2020-05-01 02:45:21 +0530

    Added Default Parameter

--- Untitled-1.js
+++ Untitled-1.js
@@ -7,5 +7,15 @@
 // const addNumbers = (number1, number2) => number1 + number2;
 // console.log(addNumbers(10, 3));
 
-const multiplyNumbers = (number1, number2) => number1 * number2;
-console.log(multiplyNumbers(5, 15));
+// const multiplyNumbers = (number1, number2) => number1 * number2;
+// console.log(multiplyNumbers(5, 15));
+
+console.log("DEFAULT PARAMETERS");
+const countdown = (start = 10) => {
+  console.log(start);
+  while (start > 0) {
+    start--;
+  }
+  console.log("Done!", start);
+};
+countdown();
