2a07e3186d771795e7d64fda83b6e9a95002f5d0
Umair896 <umairansari896@gmail.com>
2020-05-01 01:53:05 +0530

    Second Commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -4,5 +4,8 @@
 // };
 // console.log(addNumbers(10, 3));
 
-const addNumbers = (number1, number2) => number1 + number2;
-console.log(addNumbers(10, 3));
+// const addNumbers = (number1, number2) => number1 + number2;
+// console.log(addNumbers(10, 3));
+
+const multiplyNumbers = (number1, number2) => number1 * number2;
+console.log(multiplyNumbers(5, 15));
