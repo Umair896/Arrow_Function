1660f13db6d337c3925a485e14a32c631803336f
Umair896 <umairansari896@gmail.com>
2020-05-01 01:32:32 +0530

    First Commit

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+// console.log("ARROW FUNCTIONS");
+// const addNumbers = function (number1, number2) {
+//   return number1 + number2;
+// };
+// console.log(addNumbers(10, 3));
+
+const addNumbers = (number1, number2) => number1 + number2;
+console.log(addNumbers(10, 3));
